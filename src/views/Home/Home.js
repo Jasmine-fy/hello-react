ca0cb4221389b1fa0573bd7886b10e808921d4ea
SHEIN-INC\80123156 <jasmine163lover@163.com>
2022-03-17 14:23:36 +0800

    home 实现antd+改变state；list实现跨组件添加评论

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Input, Button } from "antd";
 
-class Home extends React.Component{
-  render(){
+const users = [
+  { username: "Jerry", age: 21, gender: "male" },
+  { username: "Tomy", age: 22, gender: "male" },
+  { username: "Lily", age: 19, gender: "female" },
+  { username: "Lucy", age: 20, gender: "female" },
+];
+
+class Home extends React.Component {
+  state = {
+    name: "",
+    value: "",
+  };
+  handleClick() {
+    this.setState({
+      value: this.state.name
+    })
+  }
+  handleChange(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  render() {
     return (
-      <div>LLHome</div>
-    )
+      <div>
+        {users.map((user) => {
+          return (
+            <div key={user.username}>
+              <div>姓名： {user.username}</div>
+              <div>年龄： {user.age}</div>
+              <div>性别： {user.gender}</div>
+              <hr />
+            </div>
+          );
+        })}
+
+        <div>
+          <div>{this.state.name}</div>
+          <Input value={this.state.name} onChange={this.handleChange.bind(this)} />
+          <Button type="primary" onClick={this.handleClick.bind(this)}>
+            提交
+          </Button>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Home
+export default Home;
--- src/views/List/List.js
+++ src/views/List/List.js
@@ -1,11 +1,95 @@
-import React from 'react'
+import React from "react";
+import { Form, Input, Button, message } from "antd";
+import PropTypes from "prop-types";
 
-class List extends React.Component{
-  render(){
+class CommentInput extends React.Component {
+  state = {
+    username: "",
+    content: "",
+  };
+  handleSubmit = () => {
+    if (this.props.onSubmit) {
+      const { username, content } = this.state;
+      this.props.onSubmit({ username, content });
+    }
+    this.setState({ content: "" });
+  };
+  render() {
     return (
-      <div>LLList</div>
-    )
+      <Form>
+        <Form.Item label="用户名" name="name">
+          <Input
+            value={this.state.username}
+            onChange={(e) => this.setState({ username: e.target.value })}
+          />
+        </Form.Item>
+        <Form.Item label="评论内容" name="content">
+          <Input
+            value={this.state.content}
+            onChange={(e) => this.setState({ content: e.target.value })}
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 18, span: 6 }}>
+          <Button type="primary" onClick={this.handleSubmit}>
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    );
   }
 }
 
-export default List
+class CommentList extends React.Component {
+  static defaultProps = {
+    comments: [],
+  };
+  static propTypes = {
+    comments: PropTypes.array.isRequired,
+  };
+  render() {
+    const { comments } = this.props;
+    return (
+      <div>
+        {comments.map((comment, i) => {
+          return (
+            <div key={i}>
+              {comment.username}： {comment.content}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+class CommentApp extends React.Component {
+  state = {
+    comments: [],
+  };
+  handleSubmit = (commit) => {
+    if (!commit) return;
+    if (!commit.username) {
+      message.error("请输入username");
+      return;
+    }
+    if (!commit.content) {
+      message.error("请输入content");
+      return;
+    }
+
+    this.state.comments.push(commit);
+    this.setState({
+      comments: this.state.comments,
+    });
+  };
+  render() {
+    return (
+      <div className="comment-wrapper">
+        <CommentInput onSubmit={this.handleSubmit} />
+        <CommentList comments={this.state.comments} />
+      </div>
+    );
+  }
+}
+
+export default CommentApp;
